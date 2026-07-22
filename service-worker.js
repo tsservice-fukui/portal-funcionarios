2addf6d55dfb689a3a83c28f8f8a0dc176825a31
tsservice-fukui <f.hirata@tsservice.ne.jp>
2026-07-22 12:31:26 +0900

    Configura portal como PWA

---
+++ service-worker.js
@@ -0,0 +1,81 @@
+const CACHE_NAME = "portal-tss-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./js/app.js",
+  "./manifest.webmanifest",
+  "./pages/veiculo.html",
+  "./pages/documentos.html",
+  "./pages/endereco.html",
+  "./assets/images/logo.png",
+  "./assets/images/documento-veicular.jpeg",
+  "./assets/icons/apple-touch-icon.png",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((cacheName) => cacheName !== CACHE_NAME)
+            .map((cacheName) => caches.delete(cacheName))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  const requestUrl = new URL(request.url);
+
+  if (
+    request.method !== "GET" ||
+    requestUrl.origin !== self.location.origin
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        if (response.ok) {
+          const responseCopy = response.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(request, responseCopy);
+          });
+        }
+
+        return response;
+      })
+      .catch(async () => {
+        const cachedResponse = await caches.match(request);
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        if (request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+
+        return Response.error();
+      })
+  );
+});
